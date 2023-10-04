1b4b1a40c42e265329c9ac2ac0850b396c27e461
Abhay <jayabhay9@gmail.com>
2023-10-05 00:24:03 +0530

    Fixing rendering issue

--- web/src/containers/Test.js
+++ web/src/containers/Test.js
@@ -1,40 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
-import axios from 'axios';
-import videojs from "video.js";
-import "video.js/dist/video-js.css";
-import { random_country_cords } from "../utils/country-bounding-data";
 
 const Test = () => {
-    const countries = [
-        // {
-        //     "name": "United States of America",
-        //     "count": 5755
-        // },
-    ]
-
-    const fetchCords = () => {
-        axios({
-            url: "http://de1.api.radio-browser.info/json/stations/topvote",
-            method: "POST",
-            data: {
-                order: "name",
-                hidebroken: true,
-                // limit: 500
-            }
-        })
-            .then(async res => {
-                console.log("res", res.data);
-                console.log("count", res.data.length);
-            })
-            .catch(error => {
-                console.error("Error fetching station data:", error);
-            });
-    }
-
-
     return (
         <div>
-            <button onClick={() => fetchCords(0)}>Click</button>
         </div >
     )
 }
